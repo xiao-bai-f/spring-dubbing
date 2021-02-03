74bb217c6a8fcc7771a844a8e0c5f4de8fc45503
刘楚峰 <1131211503@qq.com>
2021-02-03 23:21:33 +0800

    基础搭建

--- dubbing-model/src/main/java/dubbing/DubbingStart.java
+++ dubbing-model/src/main/java/dubbing/DubbingStart.java
@@ -2,8 +2,10 @@ package dubbing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dubbing","test"})
 public class DubbingStart {
     public static void main(String[] args) {
         SpringApplication.run(DubbingStart.class,args);
